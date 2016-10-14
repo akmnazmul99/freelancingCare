6ae9ba5e6b873a905bc3893f112378e00512a8fd
A.K.M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2016-10-14 22:37:10 +0600

    primary completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-	$('#header-menu').slicknav();
+$(document).ready(function() {
+//  wow js animation
+    wow = new WOW(
+            {
+                animateClass: 'animated',
+                offset: 100,
+            }
+    );
+    wow.init();
+
+// carousel timer
+    $(function() {
+        $('.carousel').carousel({
+            interval: 2000
+        });
+    });
 });
